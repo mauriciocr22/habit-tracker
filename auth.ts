c869d46b6e89fa0629155c1bf518e57a8f3ff276
Maurício Rodrigues <mauriciocr223@gmail.com>
2026-05-18 14:09:00 -0300

    feat: implement authentication with credentials provider and add Prisma integration

--- auth.ts
+++ auth.ts
@@ -1,7 +1,26 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcryptjs";
 
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
-    providers: [],
+    providers: [
+        Credentials({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                const { email, password } = credentials as { email: string; password: string };
+                const user = await prisma.user.findUnique({ where: { email } });
+                if (!user) return null;
+                const isPasswordValid = await bcrypt.compare(password, user.password);
+                if (!isPasswordValid) return null;
+                return { id: user.id, name: user.name, email: user.email };
+            }
+        })
+    ],
 })
