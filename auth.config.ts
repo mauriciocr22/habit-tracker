09203848165d2ef38de5581b6c9343cc2f9784f6
Maurício Rodrigues <mauriciocr223@gmail.com>
2026-05-19 16:17:19 -0300

    feat: enhance authorization logic for protected routes in auth configuration

--- auth.config.ts
+++ auth.config.ts
@@ -7,12 +7,24 @@ export const authConfig = {
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
-            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-            if (isOnDashboard) {
+            const isOnProtectedRoute =
+                nextUrl.pathname.startsWith('/dashboard') ||
+                nextUrl.pathname.startsWith('/habits') ||
+                nextUrl.pathname.startsWith('/history');
+
+            if (isOnProtectedRoute) {
                 return isLoggedIn;
             }
             return true;
         },
+        jwt({ token, user }) {
+            if (user) token.id = user.id;
+            return token;
+        },
+        session({ session, token }) {
+            session.user.id = token.id as string;
+            return session;
+        }
     },
     providers: [],
 } satisfies NextAuthConfig;
