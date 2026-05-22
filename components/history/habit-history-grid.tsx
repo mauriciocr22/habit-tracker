07045bdb42d37013816fad69ffede3f37de9de29
Maurício Rodrigues <mauriciocr223@gmail.com>
2026-05-22 18:51:13 -0300

    feat: integrate calculateStreak function into HabitHistoryGrid for habit streak tracking

--- components/history/habit-history-grid.tsx
+++ components/history/habit-history-grid.tsx
@@ -1,3 +1,5 @@
+import { calculateStreak } from "@/lib/streak";
+
 type HabitHistoryGridProps = {
     habits: {
         id: string;
@@ -27,22 +29,30 @@ export default async function HabitHistoryGrid({ habits, logs }: HabitHistoryGri
     return (
         <div>
             {
-                habits.map(habit => (
-                    <div key={habit.id}>
-                        <span>{habit.name}</span>
-                        <div className="flex gap-1"> {/* container dos quadradinhos */}
-                            {days.map(day => {
-                                const isCompleted = logs.some(
-                                    log => log.habitId === habit.id &&
-                                        log.date.toDateString() === day.toDateString()
-                                )
-                                return (
-                                    <div key={day.toISOString()} className={`h-6 w-6 ${isCompleted ? 'bg-green-500' : 'bg-gray-300'}`}></div>
-                                )
-                            })}
+                habits.map(habit => {
+                    const habitLogs = logs.filter(log => log.habitId === habit.id);
+                    const streak = calculateStreak(habitLogs);
+                    return (
+                        <div key={habit.id}>
+                            <div className="flex gap-2">
+                                <span>{habit.name}</span>
+                                <span>Streak: {streak}</span>
+                            </div>
+                            <div className="flex gap-1">
+                                {
+                                    days.map(day => {
+                                        const isCompleted = logs.some(
+                                            log => log.habitId === habit.id &&
+                                                log.date.toDateString() === day.toDateString()
+                                        )
+                                        return (
+                                            <div key={day.toISOString()} className={`h-6 w-6 ${isCompleted ? 'bg-green-500' : 'bg-gray-300'}`}></div>
+                                        )
+                                    })}
+                            </div>
                         </div>
-                    </div>
-                ))
+                    )
+                })
             }
         </div>
     )
